4aa3b8b17d552ea875922655d6ffa4a9440906b3
pippen69 <84576402+pippen69@users.noreply.github.com>
2024-03-15 17:37:36 -0400

    Update schedule.c

--- schedule.c
+++ schedule.c
@@ -12,8 +12,8 @@ struct Process{
 	int modem;
 	in cds;	
 };
-			//dispatcher== file name
-void fcfs (const char *dispatcher, Process *process, int num);//num == number of processes 
+			
+void fcfs (const char *dispatch.txt, Process *process, int num);//num == number of processes 
 void userfeedback(Process *process, int num);
 void roundrobin(Process *process, int num);
 void mixedsched(Process *process, int num);
@@ -26,9 +26,9 @@ int main(){
 Process process;
 
 printf("Enter the address of the file 'Dispatcher List' ");
-scan ("%s", dispatcher);
+scan ("%s", dispatch.txt);
 
-dispatchlist(dispatch, process, &num);
+dispatchlist(dispatch.txt, process, &num);
 
 fcfs (process, num);
 userfeedback(process, num);
@@ -43,12 +43,20 @@ return 0;
 
 
 void fcfs (const char *dispatcher, Process *process, int num){
+printf("FCFS for real-time process");
+for (int i = 0; i < num; i++){
+if (process[i].priority == 0){
+printf("Job %d: arrival time: %d, prirority: %d, requiring %d second(s) of CPU time and %d Mbytes memory. %d printer, %d scanners, %d modem, %d CD", i, arrivalime, priority, processortime, mbytes, printers, scanners, modem, cds);
+}
+}
 }
 
 void userfeedback(Process *process, int num){
+printf("User Feedback scheduler ");
 }
 
 void roundrobin(Process *process, int num){
+
 }
 
 void mixedsched(Process *process, int num){
@@ -62,5 +70,6 @@ void memorymanagement(Process *process, int num){
 
 void combo(Process *process, int num){
 }
+
 
 
